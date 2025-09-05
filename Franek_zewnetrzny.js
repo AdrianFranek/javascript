018f5c51ab3baa9b87efdca48197dc12a5ef0f25
AdrianFranek <kserator258@protonmail.com>
2025-09-05 13:42:08 +0200

    Update Franek_zewnetrzny.js

--- Franek_zewnetrzny.js
+++ Franek_zewnetrzny.js
@@ -1,2 +1,2 @@
-document.write("ostatnia modyfikacja strony".strike().fontcolor("red").fontsize(6)+"<br>");
-document.write(document.lastModified);
+document.write("ostatnia modyfikacja strony".strike().fontcolor("green").fontsize(6)+"<br>");
+document.write(document.lastModified.strike().fontcolor("green").fontsize(6));
